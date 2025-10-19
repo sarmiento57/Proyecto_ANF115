526f11de2796dc39d9b6434bc0a610bfe37407b8
Andrés Medrano <mm21030@ues.edu.sv>
2025-10-19 14:55:15 -0600

    Mensaje en landing
    estilos en settings.py

--- stela/views.py
+++ stela/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from accounts.decorators import access_required
+
 
 # Create your views here.
 def landing(request):
     return render(request, "stela/landing.html")
 
-@login_required
+@access_required('010')
 def dashboard(request):
     return render(request, "dashboard/dashboard.html")
